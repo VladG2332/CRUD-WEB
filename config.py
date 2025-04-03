609d93b192f6764abfb58f013a919ffee1563b75
Vladislav <l21590080@sjuanrio.tecnm.mx>
2025-04-02 18:06:05 -0600

    Version Estable

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://root:te9GmgGXIUlsC0vyP8Mx55o1VYbFIe0d@dpg-cups6bdsvqrc73f5b4gg-a.oregon-postgres.render.com/cetech_oj50')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
